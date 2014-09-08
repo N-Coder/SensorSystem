41fc094acf52af3f3c344aac5f017e5a1bbc2825
Niko Fink <nfink95@gmail.com>
2014-09-08 11:22:21 +0200

    Made ThreadPoolManager implement Executor

--- core/src/main/java/de/ncoder/sensorsystem/manager/timed/ThreadPoolManager.java
+++ core/src/main/java/de/ncoder/sensorsystem/manager/timed/ThreadPoolManager.java
@@ -3,11 +3,9 @@ package de.ncoder.sensorsystem.manager.timed;
 import de.ncoder.sensorsystem.AbstractComponent;
 import de.ncoder.sensorsystem.Key;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
-public class ThreadPoolManager extends AbstractComponent  {
+public class ThreadPoolManager extends AbstractComponent implements Executor {
     public static final Key<ThreadPoolManager> KEY = new Key<>(ThreadPoolManager.class);
 
     private final ExecutorService executor;
@@ -16,6 +14,7 @@ public class ThreadPoolManager extends AbstractComponent  {
         this.executor = executor;
     }
 
+    @Override
     public void execute(Runnable command) {
         executor.execute(command);
     }
@@ -30,6 +29,19 @@ public class ThreadPoolManager extends AbstractComponent  {
 
     public Future<?> submit(Runnable task) {
         return executor.submit(task);
+    }
+
+    @Override
+    public boolean isActive() {
+        return super.isActive() && !executor.isShutdown();
+    }
+
+    public boolean awaitTermination(long timeout, TimeUnit unit) throws InterruptedException {
+        return executor.awaitTermination(timeout, unit);
+    }
+
+    public boolean isTerminated() {
+        return executor.isTerminated();
     }
 
     public Runnable awakeWrapper(Runnable runnable) {
